2b693308895a8fa15153c264049154698ea1d5b4
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-01-27 23:23:32 -0300

    Create SignIn page

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,69 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { 
+  Container, 
+  InputArea, 
+  CustomButton, 
+  CustomButtonText, 
+  SignMessageButton, 
+  SignMessageButtonText, 
+  SignMessageButtonTextBold 
+} from './styles';
+
+import SignInput from '../../components/SignInput';
+
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+
+  const navigation = useNavigation();
+
+  const [ emailField, setEmailField ] = useState('');
+  const [ passwordField, setPasswordField ] = useState('');
+
+  const handleSignClick = () => {
+
+  }
+  
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignUp'}]
+    });
+  }
+
   return (
     <Container>
-      <Text>SignIn</Text>
+      <BarberLogo width="100%" height="160" />
+
+      <InputArea>
+        <SignInput 
+          IconSvg={EmailIcon} 
+          placeholder="Digite sua e-mail"
+          value={emailField}
+          onChangeText={t=>setEmailField(t)}
+        />
+
+        <SignInput 
+          IconSvg={LockIcon} 
+          placeholder="Digite sua senha"
+          value={passwordField}
+          onChangeText={t=>setPasswordField(t)}
+          password={true}
+        />
+
+        <CustomButton onPress={handleSignClick}>
+          <CustomButtonText>LOGIN</CustomButtonText>
+        </CustomButton>
+
+      </InputArea>
+
+      <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+      <SignMessageButton onPress={handleMessageButtonClick}>
+        <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
+
     </Container>
   );
 }
